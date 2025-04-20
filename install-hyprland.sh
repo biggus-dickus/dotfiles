7bd86a8362cf289bd3b9ab86b9a26ae1038a09b0
maxm <lynchnost@gmail.com>
2025-04-20 14:26:48 +0300

    Add hypr conf

---
+++ install-hyprland.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#sudo pacman -S hyprland hyprlock hypridle waybar
+
+ln -s ./hypr/hypridle.conf ~/.config/hypr/hypridle.conf
+ln -s ./hypr/hyprlock ~/.config/hypr/hyprlock.conf
+ln -s ./hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+
+#ln -s ./waybar/style.css ~/.config/waybar/style.css
