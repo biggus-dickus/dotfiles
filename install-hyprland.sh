e96d0cdb72b316b6a6598db4174ef30d8114467a
maxm <lynchnost@gmail.com>
2025-04-20 15:25:03 +0300

    Update install-hyprland script

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-#sudo pacman -S hyprland hyprlock hypridle waybar
+sudo pacman -S hyprland hyprlock hypridle waybar
 
-ln -s ./hypr/hypridle.conf ~/.config/hypr/hypridle.conf
-ln -s ./hypr/hyprlock ~/.config/hypr/hyprlock.conf
-ln -s ./hypr/hyprland.conf ~/.config/hypr/hyprland.conf
-
-#ln -s ./waybar/style.css ~/.config/waybar/style.css
+cp -rv ./hypr/* ~/.config
