bfc7e11f2c24afcd9e10d3251146964687d0ce0d
maxm <lynchnost@gmail.com>
2025-04-20 22:06:23 +0300

    Update hypr install (stupid idiot)

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -2,5 +2,5 @@
 
 sudo pacman -S dunst hyprland hyprlock hypridle waybar wofi
 
-cp -rv ./hypr/* ~/.config
-cp -rf waybar/* ~/.config
+cp -rv ./hypr/* ~/.config/hypr
+cp -rv ./waybar/* ~/.config/waybar
