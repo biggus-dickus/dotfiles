3f21f2ce1e94959386bd02333f1e26ceaa5c16b5
maxm <lynchnost@gmail.com>
2025-04-20 22:15:42 +0300

    update hypr-install

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -S dunst hyprland hyprlock hypridle waybar wofi
+sudo pacman -S dunst hyprland hyprlock hypridle otf-font-awesome waybar wofi 
 
 cp -rv ./hypr/* ~/.config/hypr
 cp -rv ./waybar/* ~/.config/waybar
