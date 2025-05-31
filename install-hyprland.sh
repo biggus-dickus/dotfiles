76849a3b063ddbd39e13886bce7a24f84358872e
maxm <lynchnost@gmail.com>
2025-05-31 20:36:30 +0300

    Hypr/waybar configs before ricing

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -S dunst hyprland hyprlock hypridle otf-font-awesome waybar wofi 
+sudo pacman -S dunst hyprland hyprlock hypridle otf-font-awesome swaybg waybar wofi xdg-desktop-portal-hyprland 
 
 cp -rv ./hypr/* ~/.config/hypr
 cp -rv ./waybar/* ~/.config/waybar
